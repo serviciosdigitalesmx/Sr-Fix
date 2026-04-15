f6c0e376c4495ba9cb2ec1fcecdbd2a7f11e0415
serviciosdigitalesmx <jesusvilla091921@gmail.com>
2026-04-15 15:37:50 -0600

    feat: implementar arquitectura centralizada con config.js v102 estable

--- config.js
+++ config.js
@@ -1,16 +1,1 @@
-// Configuración central - UNIFICADA
-window.SRFIX_CONFIG = window.SRFIX_CONFIG || {};
-window.SRFIX_CONFIG.BACKEND_URL = (function() {
-  var stored = localStorage.getItem('srfix_backend_url');
-  if (stored) return stored;
-  return 'https://script.google.com/macros/s/AKfycbw49B0GeqyZ2Yr0a-IZNqUhrhUBH0yldSO274EDHBU9gT5SPrXSs2ixIhwD5BRmg-6W/exec';
-})();
-
-// Compatibilidad con código legacy que usa SRFIX_BACKEND_URL
-window.SRFIX_BACKEND_URL = window.SRFIX_CONFIG.BACKEND_URL;
-
-// Sucursal activa
-window.SRFIX_CONFIG.getSucursalActiva = function() {
-  return localStorage.getItem('srfix_sucursal_activa') || 'GLOBAL';
-};
-window.SRFIX_CONFIG.WHATSAPP = '528117006536';
+const CONFIG = { API_URL: "https://script.google.com/macros/s/AKfycbw49B0GeqyZ2Yr0a-IZNqUhrhUBH0yldSO274EDHBU9gT5SPrXSs2ixIhwD5BRmg-6W/exec" };

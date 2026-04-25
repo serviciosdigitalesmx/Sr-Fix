00df315a887d07afb279bc4f83ac7223e2cc1790
serviciosdigitalesmx <jesusvilla091921@gmail.com>
2026-04-25 12:22:38 -0600

    Fix GitHub Pages project path for portal URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     API_URL: window.SRFIX_API_URL || localStorage.getItem('srfix_api_url') || 'https://script.google.com/macros/s/AKfycbw49B0GeqyZ2Yr0a-IZNqUhrhUBH0yldSO274EDHBU9gT5SPrXSs2ixIhwD5BRmg-6W/exec',
-    APP_URL: window.SRFIX_APP_URL || localStorage.getItem('srfix_app_url') || 'https://serviciosdigitalesmx.github.io',
+    APP_URL: window.SRFIX_APP_URL || localStorage.getItem('srfix_app_url') || 'https://serviciosdigitalesmx.github.io/Sr-Fix',
     FRONT_PASSWORD: window.SRFIX_FRONT_PASSWORD || localStorage.getItem('srfix_front_password') || 'Admin1'
 };
 window.CONFIG = CONFIG;
